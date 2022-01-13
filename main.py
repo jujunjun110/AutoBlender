99a336af53ef76861715358605d9c50ae910a78e
Jun Ito <jujunjun110@gmail.com>
2022-01-13 19:04:39 +0900

    Update: bpy fakeを入れた

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ def main():
     print(sys.version)
     path = "./assets/shoe.fbx"
     print(path)
-    print(bpy)
+    # print(bpy)
     bpy.ops.import_scene.fbx(filepath=path)
 
 
+    scene = bpy.context.scene
+
+    foo_objs = [obj for obj in scene.objects]
+
+    for obj in foo_objs:
+        print(obj)
+        print(obj.name)
 
 if __name__ == "__main__":
     main()
